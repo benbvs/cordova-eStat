98f3dd875bfe326523cbc534a0cfc3ed4464f93f
Benjamin <beauvais.benjamin@hotmail.fr>
2016-05-31 14:39:17 +0200

    Update estat.m

--- ios/estat.m
+++ ios/estat.m
@@ -4,13 +4,15 @@
 
 @implementation estat
 
+eStatAudienceTagger* audienceTagger;
+
 - (void)initEstat:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
     NSString* serial = [command.arguments objectAtIndex:0];
 
     if (serial != nil && [serial length] > 0) {
-		eStatAudienceTagger* audienceTagger = [eStatAudienceTagger eStatAudienceTaggerWithSerial:serial withCustomerData:@""];
+		audienceTagger = [eStatAudienceTagger eStatAudienceTaggerWithSerial:serial withCustomerData:@""];
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:serial];
 		NSLog(@"Hello %@!", eStatVersion);
     } else {
